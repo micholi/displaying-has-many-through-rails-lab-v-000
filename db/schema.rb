ec3490505277033299667612cfb5ed5a7c7bf0b9
Michelle Olivieri <michelleo72@gmail.com>
2018-06-06 15:05:00 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180606144409) do
+ActiveRecord::Schema.define(version: 20180606150434) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "appointment_datetime"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 20180606144409) do
   create_table "doctors", force: :cascade do |t|
     t.string "name"
     t.string "department"
+  end
+
+  create_table "patients", force: :cascade do |t|
+    t.string  "name"
+    t.integer "age"
   end
 
 end
