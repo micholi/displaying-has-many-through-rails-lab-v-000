c9913134b14eec76d3ade7f024c2284e5ada8187
Michelle Olivieri <michelleo72@gmail.com>
2018-06-06 14:55:42 +0000

    automatically backed up by learn

--- db/migrate/20180606144409_create_appointments.rb
+++ db/migrate/20180606144409_create_appointments.rb
@@ -1,7 +1,7 @@
 class CreateAppointments < ActiveRecord::Migration
   def change
     create_table :appointments do |t|
-      t.dateime :appointment_datetime
+      t.datetime :appointment_datetime
       t.integer :patient_id
       t.integer :doctor_id
     end
